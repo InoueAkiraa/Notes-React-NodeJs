3940f7af2ba04a62914b7ed311c1356022925ab0
InoueAkiraa <akiraij@hotmail.com>
2023-04-10 17:01:38 -0400

    Atualizações do código

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,46 @@
-import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+import {FiPlus, FiSearch, FiServer} from 'react-icons/fi';
+import {Container, Brand, Menu, Search, Content, NewNote} from "./styles";
+
 import {Header} from "../../components/Header";
+import {Input} from "../../components/Input";
+import {Note} from "../../components/Note";
+import {Section} from "../../components/Section";
+import {ButtonText} from "../../components/ButtonText";
 
 export function Home(){
     return(
-        <Contaner>
+        <Container>
             <Brand>
-                <h1>Rocket Notes</h1>
+                <h1>Bloco de Notas</h1>
             </Brand>
 
             <Header/>
             
             <Menu>
-            
+              <li><ButtonText title="Todos" isActive/></li>
+              <li><ButtonText title="React"/></li>
+              <li><ButtonText title="Node"/></li>
             </Menu>
 
             <Search>
-
+              <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
             </Search>
 
             <Content>
-
+              <Section title="Minhas Notas">
+                <Note data={{
+                  title: 'React', 
+                  tags: [
+                    {id: '1', name: 'react'}
+                  ]
+                }}
+                />
+              </Section>
             </Content>
-
             <NewNote>
-
+              <FiPlus/>
+              Criar Nota
             </NewNote>
-        </Contaner>
+        </Container>
     );
 }
