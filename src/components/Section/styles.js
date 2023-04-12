f73341e4c7f9535e8612d4a13ed15eecb3f47225
InoueAkiraa <akiraij@hotmail.com>
2023-04-12 16:50:42 -0400

    Iniciando a criação da página SignIn

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.section`
         padding-bottom: 16px;
         margin-bottom: 28px;
 
-        color: ${({theme}) => theme.COLORS.BACKGROUND_100};
+        color: ${({theme}) => theme.COLORS.GRAY_100};
         font-size: 20px;
         font-weight: 400;
     }
